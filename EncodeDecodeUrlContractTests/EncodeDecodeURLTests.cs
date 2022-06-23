92db29ce05067356d198cfa96e9f84a3722ea1ec
dburke-FootballPools <dominique.burke@footballpools.com>
2022-06-23 19:50:07 +0100

    comments for unit tests

--- EncodeDecodeUrlContractTests/EncodeDecodeURLTests.cs
+++ EncodeDecodeUrlContractTests/EncodeDecodeURLTests.cs
@@ -5,9 +5,23 @@ namespace EncodeDecodeUrlContractTests
     public class EncodeDecodeURLTests
     {
         [Fact]
-        public void Test1()
+        public void EncodeUrl()
         {
-            var json = @"";
+            // TODO: 
+            // json body with url in body 
+            // call encode trigger with json as param 
+            // return encoded url 
+            // Test
+        }
+
+        [Fact]
+        public void DecodeUrl()
+        {
+            // TODO: 
+            // json body with short url in body 
+            // call decode trigger with json as param 
+            // return decoded url 
+            // Test
         }
     }
 }
